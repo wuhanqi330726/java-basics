509a567021fd67d5d237eb4f42e3b65604f34555
fofa <104307147+wuhanqi330726@users.noreply.github.com>
2024-08-24 13:06:29 +0800

    ---

--- src/练习题/算法/code01.java
+++ src/练习题/算法/code01.java
@@ -1,13 +1,13 @@
 package Á·Ï°Ìâ.Ëã·¨;
 
 public class code01 {
-    public static void print(int i){
-        for (int a = 31 ;i>0;i--){
-            System.out.print((a&(1<<i))==0?"0":"1");
+    public static void print(int num){
+        for (int a = 31 ;a>=0;a--){
+            System.out.print((num & (1<<a) )==0?"0":"1");
         }
     }
     public static void main(String[] args) {
-        int i = 808020;
+        int i = 8989;
         print(i);
     }
 }
